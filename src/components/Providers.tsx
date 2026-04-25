646860ed678856ab7c303cda1de9153ab161c0cd
florencia rodriguez <florenciar504@gmail.com>
2026-04-24 23:00:42 -0300

    Add scroll animation

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -3,12 +3,14 @@
 import { type ReactNode } from 'react'
 import { CartProvider } from '@/context/cart'
 import CartDrawer from '@/components/Cart/drawer'
+import ScrollReveal from '@/components/ScrollReveal'
 
 export default function Providers({ children }: { children: ReactNode }) {
 	return (
 		<CartProvider>
 			{children}
 			<CartDrawer />
+			<ScrollReveal />
 		</CartProvider>
 	)
 }
